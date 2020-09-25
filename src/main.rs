02d115d05830084b0a6810c16c635f9be1d9bbef
Restioson <restiosondev@gmail.com>
2020-09-25 22:57:34 +0200

    try to make shader

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use crate::map::MapGenerator;
+use crate::map::mesh::MapGenerator;
+use crate::map::shader::{MyMaterial, VERTEX_SHADER, FRAGMENT_SHADER};
 use bevy::prelude::*;
 use bevy::window::WindowMode;
 
@@ -6,9 +7,13 @@ mod map;
 mod rts_camera;
 
 use rts_camera::rts_camera_system;
+use bevy::render::render_graph::{RenderGraph, AssetRenderResourcesNode, base};
+use bevy::render::pipeline::{PipelineDescriptor, PipelineSpecialization, DynamicBinding, RenderPipeline};
+use bevy::render::shader::{ShaderStages, ShaderStage};
 
 fn main() {
     App::build()
+        .add_default_plugins()
         .add_resource(Msaa { samples: 4 })
         .add_resource(WindowDescriptor {
             vsync: true,
@@ -16,7 +21,7 @@ fn main() {
             mode: WindowMode::BorderlessFullscreen,
             ..Default::default()
         })
-        .add_default_plugins()
+        .add_asset::<MyMaterial>()
         .add_startup_system(setup.system())
         .add_system(rts_camera_system.system())
         .run();
@@ -24,21 +29,67 @@ fn main() {
 
 fn setup(
     mut commands: Commands,
-    mut materials: ResMut<Assets<StandardMaterial>>,
     mut meshes: ResMut<Assets<Mesh>>,
+    mut pipelines: ResMut<Assets<PipelineDescriptor>>,
+    mut shaders: ResMut<Assets<Shader>>,
+    mut materials: ResMut<Assets<MyMaterial>>,
+    mut render_graph: ResMut<RenderGraph>,
 ) {
     let generator = MapGenerator::new();
     let mesh_handles = generator.generate_meshes(&mut meshes);
 
     let mut translation = Vec3::new(0.0, 0.0, 0.0);
+
+    // Create a new shader pipeline
+    let pipeline_handle = pipelines.add(PipelineDescriptor::default_config(ShaderStages {
+        vertex: shaders.add(Shader::from_glsl(ShaderStage::Vertex, VERTEX_SHADER)),
+        fragment: Some(shaders.add(Shader::from_glsl(ShaderStage::Fragment, FRAGMENT_SHADER))),
+    }));
+
+    // Add an AssetRenderResourcesNode to our Render Graph. This will bind MyMaterial resources to our shader
+    render_graph.add_system_node(
+        "my_material",
+        AssetRenderResourcesNode::<MyMaterial>::new(true),
+    );
+
+    // Add a Render Graph edge connecting our new "my_material" node to the main pass node. This ensures "my_material" runs before the main pass
+    render_graph
+        .add_node_edge("my_material", base::node::MAIN_PASS)
+        .unwrap();
+
+    // Create a new material
+    let material = materials.add(MyMaterial {
+        color: Color::rgb(0.0, 0.8, 0.0),
+    });
+
     for ((x, z), mesh) in mesh_handles {
         translation = Vec3::new(x as f32, 0.0, z as f32);
-        commands.spawn(PbrComponents {
-            mesh,
-            material: materials.add(Color::rgb(0.5, 0.4, 0.3).into()),
-            transform: Transform::from_translation(translation),
-            ..Default::default()
-        });
+        commands
+            .spawn(PbrComponents {
+                mesh,
+                render_pipelines: RenderPipelines::from_pipelines(vec![RenderPipeline::specialized(
+                    pipeline_handle,
+                    // NOTE: in the future you wont need to manually declare dynamic bindings
+                    PipelineSpecialization {
+                        dynamic_bindings: vec![
+                            // Transform
+                            DynamicBinding {
+                                bind_group: 1,
+                                binding: 0,
+                            },
+                            // MapMaterial_color
+                            DynamicBinding {
+                                bind_group: 1,
+                                binding: 1,
+                            },
+                        ],
+                        ..Default::default()
+                    },
+                )]),
+                transform: Transform::from_translation(translation),
+                ..Default::default()
+            })
+            .with(material);
     }
 
     let italy = Vec3::new(552.0, 0.0, 377.26);
---
+++ src/map/mesh.rs
@@ -0,0 +1,158 @@
+use bevy::prelude::*;
+use bevy::render::mesh::{Mesh, VertexAttribute};
+use bevy::render::pipeline::PrimitiveTopology;
+use image::{DynamicImage, GenericImageView};
+use std::fmt::{self, Debug, Formatter};
+
+static HEIGHTMAP_BYTES: &[u8] = include_bytes!("../assets/europe_heightmap.png");
+
+pub struct MapGenerator {
+    heightmap: DynamicImage,
+    resolution: u32,
+    chunk_size: u32,
+}
+
+impl MapGenerator {
+    pub fn new() -> Self {
+        let image = image::load_from_memory(HEIGHTMAP_BYTES).unwrap();
+        MapGenerator {
+            heightmap: image,
+            /// Must be one of 1, 2, 4, 5, 8, 10, 16, 20, 32, 40, 64, 80, 128, 160, 320, 640
+            chunk_size: 64,
+            /// resolution <= 255
+            resolution: 255,
+        }
+    }
+
+    pub fn generate_meshes(&self, meshes: &mut Assets<Mesh>) -> Vec<((u32, u32), Handle<Mesh>)> {
+        let (img_width, img_height) = self.heightmap.dimensions();
+        let scale = 4;
+        let x_tiles = img_width / self.chunk_size / scale;
+        let z_tiles = img_height / self.chunk_size / scale;
+
+        let mut handles = Vec::with_capacity((x_tiles * z_tiles) as usize);
+        for x in 0..x_tiles {
+            for z in 0..z_tiles {
+                let top_left = (x, z);
+                let generator = ChunkGenerator {
+                    heightmap: &self.heightmap,
+                    resolution: self.resolution as i32,
+                    side_length: self.chunk_size as i32,
+                    scale,
+                    top_left_px: (
+                        (x * self.chunk_size * scale) as i32,
+                        (z * self.chunk_size * scale) as i32,
+                    ),
+                };
+
+                let coords = (top_left.0 * self.chunk_size, top_left.1 * self.chunk_size);
+                let mesh = generator.create_mesh();
+                let handle = meshes.add(mesh);
+                handles.push((coords, handle));
+            }
+        }
+
+        handles
+    }
+}
+
+pub struct ChunkGenerator<'a> {
+    heightmap: &'a DynamicImage,
+    side_length: i32,
+    resolution: i32,
+    scale: u32,
+    top_left_px: (i32, i32),
+}
+
+impl Debug for ChunkGenerator<'_> {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        f.debug_struct("MeshGenerator")
+            .field("side_length", &self.side_length)
+            .field("resolution", &self.resolution)
+            .field("top_left_px", &self.top_left_px)
+            .finish()
+    }
+}
+
+impl ChunkGenerator<'_> {
+    #[inline]
+    pub fn sample(&self, x: i32, z: i32) -> f32 {
+        let dim = self.heightmap.dimensions();
+        let max = (dim.0 as i32 - 1, dim.1 as i32 - 1);
+        let to_img = |n, top_left| {
+            let in_chunk =
+                (n as f32 / self.resolution as f32 * self.side_length as f32 * self.scale as f32)
+                    .floor();
+            in_chunk as i32 + top_left
+        };
+        let img_x = i32::min(i32::max(0, to_img(x, self.top_left_px.0)), max.0);
+        let img_z = i32::min(i32::max(0, to_img(z, self.top_left_px.1)), max.1);
+
+        let mut red = self.heightmap.get_pixel(img_x as u32, img_z as u32)[0];
+        if red > 0 {
+            red += 20; // TODO for visibility of land w/o colour
+        }
+
+        red as f32 / 255.0 * 5.0
+    }
+
+    pub fn create_mesh(&self) -> Mesh {
+        let res = self.resolution;
+        let res_plus_1_sq = (res + 1) * (res + 1);
+        let mut positions = Vec::with_capacity(res_plus_1_sq as usize);
+        let mut normals = Vec::with_capacity(res_plus_1_sq as usize);
+        let mut indices = Vec::with_capacity((res * res * 2 * 3) as usize);
+
+        assert!((res + 1) * (res + 1) <= 1 << 16, "Resolution too large!");
+
+        for z in 0..res + 1 {
+            for x in 0..res + 1 {
+                let top_left = self.sample(x - 1, z - 1);
+                let top_right = self.sample(x + 1, z - 1);
+                let bottom_left = self.sample(x - 1, z + 1);
+                let bottom_right = self.sample(x + 1, z + 1);
+                let y = self.sample(x, z);
+
+                let x = x as f32 / self.resolution as f32 * self.side_length as f32;
+                let z = z as f32 / self.resolution as f32 * self.side_length as f32;
+
+                // Position
+                positions.push([x, y, z]);
+
+                // Normal
+                let normal_1 =
+                    Vec3::new(top_left - top_right, 2.0, bottom_left - top_left).normalize();
+                let normal_2 = Vec3::new(bottom_left - bottom_right, 2.0, top_right - bottom_right)
+                    .normalize();
+                let normal = (normal_1 + normal_2).normalize();
+                normals.push([normal.x(), normal.y(), normal.z()]);
+            }
+        }
+
+        for z in 0..res as u32 {
+            for x in 0..res as u32 {
+                let top_left = x + z * (res + 1) as u32;
+                let top_right = x + 1 + z * (res + 1) as u32;
+                let bottom_left = x + (z + 1) * (res + 1) as u32;
+                let bottom_right = x + 1 + (z + 1) * (res + 1) as u32;
+
+                indices.push(bottom_left);
+                indices.push(top_right);
+                indices.push(top_left);
+
+                indices.push(bottom_left);
+                indices.push(bottom_right);
+                indices.push(top_right);
+            }
+        }
+
+        Mesh {
+            primitive_topology: PrimitiveTopology::TriangleList,
+            attributes: vec![
+                VertexAttribute::position(positions),
+                VertexAttribute::normal(normals),
+            ],
+            indices: Some(indices),
+        }
+    }
+}
