d7229ec1d5cb71cda0173918ba59ff173e1c525a
Restioson <restiosondev@gmail.com>
2020-09-27 11:40:01 +0200

    enlarge map

--- fetch_tiles.py
+++ fetch_tiles.py
@@ -6,15 +6,15 @@ def conv(lat, long):
     x = (long + 180.0) / 360.0 * 2.0 * (3.0 ** zoom)
     y = (90.0 - lat) / 180.0 * (3.0**zoom)
 
-    return (x, y)
+    return (floor(x), floor(y))
 
-top_left = conv(65.09, -13.3)
+top_left = conv(65.09, -24.3)
 bottom_right = conv(12.36, 63.14)
 
-for x in range(floor(top_left[0]), floor(bottom_right[0]) + 1):
-    for y in range(floor(top_left[1]), floor(bottom_right[1]) + 1):
+for x in range(top_left[0], bottom_right[0] + 1):
+    for y in range(top_left[1], bottom_right[1] + 1):
         print(f"{x}, {y}")
 
         with urllib.request.urlopen(f"https://terrariumearth.azureedge.net/geo3/elevation2/3/{x}/{y}") as r:
-            with open(f"assets/heightmap/{x - 25}x{y - 3}.heightmap", "wb") as f:
+            with open(f"assets/heightmap/{x - top_left[0]}x{y - top_left[1]}.heightmap", "wb") as f:
                 f.write(r.read())
